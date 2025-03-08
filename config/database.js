205f3c8a2344fb7dee08ee43703cf8b1a9b3c80f
MehakGupta <mehakg1811@gmail.com>
2025-03-08 19:49:53 +0530

    database updated

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Mansi@567',
+    password: 'root',
     database: 'Localskillz'
 });
 
